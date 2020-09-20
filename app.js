7e5d7bf06a7513bbd80a7b7a1570f928e7c3957b
Hiro Kumagami <hkumagam@ucsc.edu>
2020-09-20 14:01:47 -0700

    Now the database was formless and void, and static data was over the surface of the server. And the Spirit of Mongod was hovering over the surface of the server. And Mongod said, 'Let there be MongoDB,' and there was MongoDB. And Mongod saw that the database was good, and he seperated the data from the server.

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
 //////Bring in Libraries from npm(online repo of node moddules/libraries)//////
 const express = require("express");
 const bodyParser = require("body-parser");
+MongoClient = require('mongodb').MongoClient;
+ObjectId = require('mongodb').ObjectID;
+assert = require('assert');
 //////////Create Library Objects/////////////////
 const app = express();
 /////////Use those library objects////////////
@@ -11,7 +14,21 @@ let courses = ["CSE 20", "MATH 19A", "STEV 1"];
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
-/////////////////////////////////////////////////////////////////////////////////
+
+
+/////////////////////////////////////////////Set-up/create your MONGODB DATABASE///////////////////////////////////////
+const url = 'mongodb://localhost:27017'; //database connection URL
+const dbName = 'scheduleDB'; //database dbName
+const client = new MongoClient(url, {useUnifiedTopology: true});//create a new instance of MongoClient
+////////////////////CONNECT your database(Client) to this SERVER via port 27017////////////////////////
+client.connect(function(err){
+  assert.equal(null, err);
+  console.log("Mongodb connected successfully to the server via port 27017");
+  db = client.db(dbName); //assign db object
+});
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
 ////////Load the home page "schedule.ejs" (i.e. schedule.ejs "gets" "/" from app.js)/////////
 app.get("/", function(req, res) {
   //Express (using ejs) finds & modifies the schedule.ejs file
