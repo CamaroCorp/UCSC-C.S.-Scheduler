39cf351c27e0d1e0c88237455d5a6fe884490268
Hiro Kumagami <hkumagam@ucsc.edu>
2020-09-21 13:43:57 -0700

    implement EJS into the home page

--- app.js
+++ app.js
@@ -9,7 +9,11 @@ assert = require('assert');
 const app = express();
 /////////Use those library objects////////////
 ///////////////Store courses as array (temporary)/////////////////////
-let courses = ["CSE 20", "MATH 19A", "STEV 1"];
+let q1 = ["CSE 20", "MATH 19A", "STEV 1"];
+let currentTerm = "";
+let seasons = ["Fall", "Winter", "Spring", "Summer"];
+let years = ["2019-2020", "2020-2021", "2021-2022", "2022-2023"];
+let yearCount = 3;
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
@@ -32,17 +36,39 @@ client.connect(function(err){
 ////////Load the home page "schedule.ejs" (i.e. schedule.ejs "gets" "/" from app.js)/////////
 app.get("/", function(req, res) {
   //Express (using ejs) finds & modifies the schedule.ejs file
-  res.render("schedule",{newCourseListItems: courses});
+  res.render("schedule",{
+    quarterCourses: q1,
+    seasons: seasons,
+    years: years
+  });
 });
 ////////////////////////////////////////////////////////////////////////////////
+app.post("/", function(req, res){
+  if(req.body.editYear === "add"){
+    years.push("Incomplete");
+  }
+  if(req.body.editYear === "remove"){
+    years.pop();
+  }
+  res.redirect("/");
+});
 ////////Load the edit page "edit.ejs" (i.e. edit.ejs "gets" "/edit" from app.js)/////////
 app.get("/edit", function(req, res) {
   //Express (using ejs) finds & modifies the edit.ejs file
-  res.render("edit",{newCourseListItems: courses});
+  res.render("edit",{
+    quarterCourses: q1,
+    currentTerm: currentTerm
+  });
 });
+
 ////For when a user clicks on a quarter, activating the html form "q1" (schedule.ejs)
 app.post("/edit", function(req, res){
   ////redirect to the edit page
+  let season_index = (req.body.q) %4;
+  let year_index = Math.floor(req.body.q/4);
+  let season = seasons[season_index];
+  let year = years[year_index];
+  currentTerm = season + " " + year;
   res.redirect("/edit");
 });
 
--- public/index.js
+++ public/index.js
@@ -1,24 +1,24 @@
 
 //Schedule Page
-
-var quarters = document.querySelectorAll(".quarter");
-var seasons = document.querySelectorAll(".season");
-var years = document.querySelectorAll(".year");
-var number_of_quarters = quarters.length;
-var chosen_quarter_number = 0;
-var chosen_season = "season";
-var chosen_year = "year";
-
-//Adds a click functionality to each quarter in order to redirect users to the specified "edit schedule" page
-for (var i=0; i<number_of_quarters-1; i++){
-  document.querySelectorAll(".quarter")[i].addEventListener("click", function(){
-
-    chosen_quarter_number = this.id;
-    season_index = chosen_quarter_number%4;
-    year_index = Math.floor(chosen_quarter_number/4);
-    chosen_season = seasons[season_index].innerHTML;
-    chosen_year = years[year_index].innerHTML;
-    str = "q"+ i;
-    document.forms["q1"].submit();
-    });
-}
+// 
+// var quarters = document.querySelectorAll(".quarter");
+// var seasons = document.querySelectorAll(".season");
+// var years = document.querySelectorAll(".year");
+// var number_of_quarters = quarters.length;
+// var chosen_quarter_number = 0;
+// var chosen_season = "season";
+// var chosen_year = "year";
+//
+// //Adds a click functionality to each quarter in order to redirect users to the specified "edit schedule" page
+// for (var i=0; i<number_of_quarters-1; i++){
+//   document.querySelectorAll(".quarter")[i].addEventListener("click", function(){
+//
+//     chosen_quarter_number = this.name;
+//     season_index = chosen_quarter_number%4;
+//     year_index = Math.floor(chosen_quarter_number/4);
+//     chosen_season = seasons[season_index].innerHTML;
+//     chosen_year = years[year_index].innerHTML;
+//     console.log(chosen_quarter_number);
+//     document.forms[chosen_quarter_number].submit();
+//     });
+// }
