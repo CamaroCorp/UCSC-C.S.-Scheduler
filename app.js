7b96df46b49eeb4c88ec0c2f1df36a0728bdfdeb
Hiro Kumagami <hkumagam@ucsc.edu>
2020-09-22 16:07:22 -0700

    add remove-course functionality

--- app.js
+++ app.js
@@ -9,11 +9,11 @@ assert = require('assert');
 const app = express();
 /////////Use those library objects////////////
 ///////////////Store courses as array (temporary)/////////////////////
-let q1 = ["CSE 20", "MATH 19A", "STEV 1"];
-let currentTerm = "";
+let usersCourses = [["CSE 20","MATH 19A","STEV 1"],['WRIT 1','STEV 2','CSE 30'],['INVOLUNTARY LEAVE OF ABSENSE'],['MATH 19B','MATH 21','HIS 10A','THEA 14','STAT 131','MATH 23A'],[],[],[],[],[],[],[],[],[],[],[],[]];
+let currentTerm = "Undefined";
 let seasons = ["Fall", "Winter", "Spring", "Summer"];
-let years = ["2019-2020", "2020-2021", "2021-2022", "2022-2023"];
-let yearCount = 3;
+let years = ["Freshman", "Sophomore", "Junior", "Senior"];
+let chosenTerm_index = 0;
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
@@ -37,40 +37,65 @@ client.connect(function(err){
 app.get("/", function(req, res) {
   //Express (using ejs) finds & modifies the schedule.ejs file
   res.render("schedule",{
-    quarterCourses: q1,
+    usersCourses: usersCourses,
     seasons: seasons,
     years: years
   });
 });
 ////////////////////////////////////////////////////////////////////////////////
 app.post("/", function(req, res){
-  if(req.body.editYear === "add"){
-    years.push("Incomplete");
-  }
-  if(req.body.editYear === "remove"){
+  if(req.body.name === "add-year"){
+    years.push("Super");
+    usersCourses.push([],[],[],[]);
+    res.redirect("/");
+  }else if(req.body.name === "remove-year" && years.length>4){
     years.pop();
+    usersCourses.pop([],[],[],[]);
+    res.redirect("/");
+  }else if((req.body.name === "remove-year" && years.length<=4) || (req.body.name === "add-year" && year.length>=8)){
+    res.redirect("/");
+  }else{
+    ////redirect to the edit page
+    chosenTerm_index = req.body.name;
+    console.log(chosenTerm_index);
+    let season_index = chosenTerm_index %4;
+    let year_index = Math.floor(chosenTerm_index/4);
+    let season = seasons[season_index];
+    let year = years[year_index];
+    currentTerm = season + " / " + year;
+    res.redirect("/edit");
   }
-  res.redirect("/");
 });
 ////////Load the edit page "edit.ejs" (i.e. edit.ejs "gets" "/edit" from app.js)/////////
 app.get("/edit", function(req, res) {
   //Express (using ejs) finds & modifies the edit.ejs file
+  console.log(usersCourses)
   res.render("edit",{
-    quarterCourses: q1,
-    currentTerm: currentTerm
+    currentTermCourses: usersCourses[chosenTerm_index],
+    currentTerm: (currentTerm + " Year")
   });
 });
 
-////For when a user clicks on a quarter, activating the html form "q1" (schedule.ejs)
+////For when a user clicks on a quarter, activating the html form "quarter" (schedule.ejs)
 app.post("/edit", function(req, res){
-  ////redirect to the edit page
-  let season_index = (req.body.q) %4;
-  let year_index = Math.floor(req.body.q/4);
-  let season = seasons[season_index];
-  let year = years[year_index];
-  currentTerm = season + " " + year;
-  res.redirect("/edit");
+  if (req.body.name === "add-course"){
+    res.redirect("/edit");
+  }else{
+      let index = parseInt(req.body.name);
+      console.log(index)
+      let a = usersCourses[chosenTerm_index].slice(0, index);
+      let b = usersCourses[chosenTerm_index].slice(index+1,usersCourses[chosenTerm_index].length +1);
+      if(a===[] && b===[]){
+        usersCourses[chosenTerm_index] = [];
+        res.redirect("/edit");
+      }else{
+        console.log(a, b)
+        usersCourses[chosenTerm_index] = a.concat(b);
+        res.redirect("/edit");
+      }
+  }
 });
+
 
 //////fire up the server
 app.listen(3000, function() {
